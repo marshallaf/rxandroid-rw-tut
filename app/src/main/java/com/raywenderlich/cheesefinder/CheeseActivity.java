672a4a7dd12e28878e43e329d4759bb78d0516fd
Andrew Marshall <marshall.a.f@gmail.com>
2017-06-15 21:35:53 -0500

    Replace anon classes with lambda expressions

--- app/src/main/java/com/raywenderlich/cheesefinder/CheeseActivity.java
+++ app/src/main/java/com/raywenderlich/cheesefinder/CheeseActivity.java
@@ -24,20 +24,12 @@ package com.raywenderlich.cheesefinder;
 
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Cancellable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class CheeseActivity extends BaseSearchActivity {
@@ -47,26 +39,13 @@ public class CheeseActivity extends BaseSearchActivity {
     // returns an observable that emits strings
     private Observable<String> createButtonClickObservable() {
         // create and return a new Observable
-        return Observable.create(new ObservableOnSubscribe<String>() {
-            // this defines what happens when it is subscribed to
-            @Override
-            public void subscribe(final ObservableEmitter<String> emitter) throws Exception {
-                // when the click event happens, call onNext on the emitter
-                mSearchButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        emitter.onNext(mQueryEditText.getText().toString());
-                    }
-                });
+        // this defines what happens when it is subscribed to
+        return Observable.create(emitter -> {
+            // when the click event happens, call onNext on the emitter
+            mSearchButton.setOnClickListener(view -> emitter.onNext(mQueryEditText.getText().toString()));
 
-                // this prevents memory leak by removing the reference to the listener
-                emitter.setCancellable(new Cancellable() {
-                    @Override
-                    public void cancel() throws Exception {
-                        mSearchButton.setOnClickListener(null);
-                    }
-                });
-            }
+            // this prevents memory leak by removing the reference to the listener
+            emitter.setCancellable(() -> mSearchButton.setOnClickListener(null));
         });
     }
 
@@ -81,36 +60,23 @@ public class CheeseActivity extends BaseSearchActivity {
         Observable<String> searchTextObservable = Observable.merge(textChangeStream, buttonClickStream);
 
         // subscribe to the observable with a simple consumer
+        // accept() will be called when the observable emits an item
         mDisposable = searchTextObservable
                 // start on UI thread for progress bar
                 .observeOn(AndroidSchedulers.mainThread())
                 // doOnNext() is called every time an item is emitted by an observable
-                .doOnNext(new Consumer<String>() {
-                    @Override
-                    public void accept(String s) throws Exception {
-                        showProgressBar();
-                    }
-                })
+                .doOnNext(s -> showProgressBar())
                 // switch to IO thread
                 .observeOn(Schedulers.io())
                 // for each query, return a list of results
-                .map(new Function<String, List<String>>() {
-                    @Override
-                    public List<String> apply(String query) throws Exception {
-                        return mCheeseSearchEngine.search(query);
-                    }
-                })
+                .map(query -> mCheeseSearchEngine.search(query))
                 // anything after this will be called on the UI thread
                 .observeOn(AndroidSchedulers.mainThread())
                 // since our showResult() call there updates the UI, it has to be on the UI thread
-                .subscribe(new Consumer<List<String>>() {
-                    // accept() will be called when the observable emits an item
-                    @Override
-                    public void accept(List<String> result) throws Exception {
-                        // perform search and show the results
-                        hideProgressBar();
-                        showResult(result);
-                    }
+                .subscribe(result -> {
+                    // perform search and show the results
+                    hideProgressBar();
+                    showResult(result);
                 });
     }
 
@@ -124,46 +90,35 @@ public class CheeseActivity extends BaseSearchActivity {
 
     private Observable<String> createTextChangeObservable() {
         // create an observable
-        Observable<String> textChangeObservable = Observable.create(new ObservableOnSubscribe<String>() {
-            @Override
-            public void subscribe(final ObservableEmitter<String> emitter) throws Exception {
-                // when an observer subscribes, make a TextWatcher
-                final TextWatcher watcher = new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        Observable<String> textChangeObservable = Observable.create(emitter -> {
+            // when an observer subscribes, make a TextWatcher
+            final TextWatcher watcher = new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-                    }
+                }
 
-                    // when the text changes, emit the new sequence
-                    @Override
-                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                        emitter.onNext(charSequence.toString());
-                    }
+                // when the text changes, emit the new sequence
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    emitter.onNext(charSequence.toString());
+                }
 
-                    @Override
-                    public void afterTextChanged(Editable editable) {
+                @Override
+                public void afterTextChanged(Editable editable) {
 
-                    }
-                };
+                }
+            };
 
-                // add the watcher to the query text view
-                mQueryEditText.addTextChangedListener(watcher);
+            // add the watcher to the query text view
+            mQueryEditText.addTextChangedListener(watcher);
 
-                // make sure we don't keep any references
-                emitter.setCancellable(new Cancellable() {
-                    @Override
-                    public void cancel() throws Exception {
-                        mQueryEditText.removeTextChangedListener(watcher);
-                    }
-                });
-            }
+            // make sure we don't keep any references
+            emitter.setCancellable(() -> mQueryEditText.removeTextChangedListener(watcher));
         });
         // return the observable, with a filter such that queries of size == 1 don't get propagated further
-        return textChangeObservable.filter(new Predicate<String>() {
-            @Override
-            public boolean test(String s) throws Exception {
-                return s.length() >= 2;
-            }
-        }).debounce(500, TimeUnit.MILLISECONDS);
+        return textChangeObservable
+                .filter(s -> s.length() >= 2)
+                .debounce(500, TimeUnit.MILLISECONDS);
     }
 }
